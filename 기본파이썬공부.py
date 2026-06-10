d876c1b30d78d5d022a6e6a543108d7129e4bb31
jshn2580-source <jshn2580@gmail.com>
2026-06-10 20:49:17 +0900

    데이터타입 및 if기초

---
+++ 기본파이썬공부.py
@@ -0,0 +1,53 @@
+문자변수 = """
+       띄어쓰기 하고 싶을때,
+       가독성 높이고 싶을때...
+"""
+num = 10 #숫자를 값으로 가지는 변수 num
+count = 100
+
+# print(num + count, 문자변수)
+
+list = ['사과','배']
+
+#데이터타입 확인
+#print(type(list))
+# 사칙연산자 = 곱하기는 복사기능이 있음
+#ㅔprint("Hi"*3)
+# 첫대문자 주의할 것
+a = True
+b = False
+#print(type(a))
+
+#삼항식
+age = 20
+
+# 값1 if 조건식 else 값2
+#print("성인" if age <=19 else "미성년자")
+
+# tab문법
+num = 10
+# if num > 5:
+    #print("5보다 크다")
+    #print("나는 if식에 귀속되어 있음")
+#print ("나는 위의 if식과 상관없음")
+
+
+# score = 80
+
+# if score >=60:
+#     print("합격")
+# else:
+#     print("불합격")
+
+score = 75
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print(score,"점이라서 C이군요ㅜㅜ")
+else:
+    print("F")
+    
+   
